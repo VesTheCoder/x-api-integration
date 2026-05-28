683ad95624e26d05f065c87bd54105d8b77a714b
VesTheCoder <vladislav.blackrock@gmail.com>
2026-05-28 17:32:07 +0100

    add max runtime helper for max_runtime query parameter

--- app/core/utils/__init__.py
+++ app/core/utils/__init__.py
@@ -1,3 +1,4 @@
+from app.core.utils.max_runtime import has_exceeded_max_runtime
 from app.core.utils.status_codes import raise_for_status
 
-__all__ = ["raise_for_status"]
+__all__ = ["raise_for_status", "has_exceeded_max_runtime"]
