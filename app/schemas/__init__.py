86e24b388079f79e53ff85ec50a7112f2bde9d57
VesTheCoder <vladislav.blackrock@gmail.com>
2026-05-28 17:31:24 +0100

    add unut wrapper for schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,27 @@
+from app.schemas.error import ErrorDTO
+from app.schemas.x_entities import (
+    ProviderRunMetadata,
+    XAccountInfo,
+    XAccountInfoResult,
+    XAccountsSearchMetadata,
+    XAccountsSearchResult,
+)
+from app.schemas.x_queries import (
+    GetAccountInfoQuery,
+    SearchAccountsQuery,
+    XProviderKey,
+    XQuery,
+)
+
+__all__ = [
+    "ErrorDTO",
+    "XAccountInfo",
+    "XAccountInfoResult",
+    "XAccountsSearchMetadata",
+    "XAccountsSearchResult",
+    "ProviderRunMetadata",
+    "GetAccountInfoQuery",
+    "SearchAccountsQuery",
+    "XProviderKey",
+    "XQuery",
+]
