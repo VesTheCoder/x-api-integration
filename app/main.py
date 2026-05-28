9d3d2e2036b489e8de79509431562686585f5c39
VesTheCoder <vladislav.blackrock@gmail.com>
2026-05-28 11:55:00 +0100

    add lifespan for db

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 import uvicorn
 from app.api.exception_handlers import provider_exception_handler
 from app.api.v1.x.base import router as x_router
+from app.core.database.database import close_database, init_database
 from app.core.exceptions import ProviderError
 from app.settings import Settings
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 settings = Settings()
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Manage database connection lifecycle.
+    """
+    await init_database(settings.postgres.database_url)
+    yield
+    await close_database()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_exception_handler(ProviderError, provider_exception_handler)
 app.add_middleware(
